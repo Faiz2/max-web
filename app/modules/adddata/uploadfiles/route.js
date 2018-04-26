651316b82a102c859fbcb4e9e8046a021aa5f103
WangSen <wsgoodman@163.com>
2018-04-26 19:13:28 +0800

    在application中设置beforeModel。

--- app/modules/adddata/uploadfiles/route.js
+++ app/modules/adddata/uploadfiles/route.js
@@ -1,12 +1,4 @@
 import Route from '@ember/routing/route';
 import {inject} from '@ember/service';
 export default Route.extend({
-    cookies: inject(),
-    beforeModel(transition) {
-        if (!this.controllerFor('index').get('userIsLoggedIn')) {
-            let loginController = this.controllerFor('index');
-            loginController.set('previousTransition', transition);
-            this.transitionTo('index');
-        }
-    },
 });
