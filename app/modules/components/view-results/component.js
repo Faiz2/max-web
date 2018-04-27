0f2d297c45409e70129d56db1bba90d409b0adfe
WangSen <wsgoodman@163.com>
2018-04-27 19:36:15 +0800

    修改弹窗的样式，对保存结果按钮添加了新逻辑。
    弹窗样式大致修改，未完全到细节。保存结果逻辑未完成。

--- app/modules/components/view-results/component.js
+++ app/modules/components/view-results/component.js
@@ -1,14 +1,23 @@
 import Component from '@ember/component';
 import {set} from '@ember/object';
-
+import {later} from '@ember/runloop';
 export default Component.extend({
     isSave: false,
+    allMonth: false,
+    saveState: false,
     actions: {
         saveData() {
             set(this, 'isSave', true)
         },
+        chooseAllMonth() {
+            set(this, 'allMonth',true);
+        },
         confirmSave() {
             set(this, 'isSave', false);
+            set(this, 'saveState', true);
+            later(()=> {
+                set(this, 'saveState', false);
+            },1800)
         },
     },
 });
